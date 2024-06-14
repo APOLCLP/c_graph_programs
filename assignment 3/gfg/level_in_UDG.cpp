e942bdd012c83dffd360484fa010360043ed0690
Aman <amrj0880@gmail.com>
2024-06-14 05:46:46 +0530

    Update level_in_UDG.cpp

--- assignment 3/gfg/level_in_UDG.cpp
+++ assignment 3/gfg/level_in_UDG.cpp
@@ -3,33 +3,23 @@ int nodeLevel(int V, vector<int> adj[], int X)
 	    int vis[V]={0};
        
        
-       int level=0;
-       queue<int> q;
-       q.push(0);
+       
+       queue<pair<int,int>> q;
+       q.push({0,0});
        
        while(!q.empty()){
+           int num = q.front().first;
+           int level = q.front().second;
+           q.pop();
+           if(num==X) return level;
            
-           
-            
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int parent = q.front();
-                
-                q.pop();
-                if(parent ==X)return level;
-                
+               for(auto it :adj[num]){
+                    if(!vis[it]){
+                        vis[it]=1;
+                        q.push({it,level+1});
+                    }
 
-               for(auto it :adj[parent]){
-               if(!vis[it]){
-                  vis[it]=1;
-                  q.push(it);
-               }
-               
-           }
-           
-
-
-            }level++;
+            }
            
            
            
